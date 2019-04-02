d3d2b2aec4cf83c8a7d349875899cfa6839a3851
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-04-02 18:00:23 +0900

    Make logger initialization simpler

--- src/bin/fbx-viewer/main.rs
+++ src/bin/fbx-viewer/main.rs
@@ -1,7 +1,5 @@
 //! FBX viewer.
 
-use std::env;
-
 use fbx_viewer::CliOpt;
 use log::info;
 use structopt::StructOpt;
@@ -9,26 +7,9 @@ use structopt::StructOpt;
 pub mod vulkan;
 
 fn main() {
-    setup_logger();
+    env_logger::from_env(env_logger::Env::default().default_filter_or("debug")).init();
     info!("version: {}", env!("CARGO_PKG_VERSION"));
 
     let opt = CliOpt::from_args();
     vulkan::main(opt).expect("Vulkan mode failed");
-}
-
-fn setup_logger() {
-    /// Default log level.
-    const DEFAULT_LOG_LEVEL: &str = "debug";
-    /// Envvar name for the logger.
-    const LOG_VAR: &str = "RUST_LOG";
-
-    let underscored_name = env!("CARGO_PKG_NAME").replace('-', "_");
-    let defval = format!("{}={}", underscored_name, DEFAULT_LOG_LEVEL);
-
-    let newval = match env::var(LOG_VAR) {
-        Ok(v) => format!("{},{}", defval, v),
-        Err(_) => defval,
-    };
-    env::set_var(LOG_VAR, &newval);
-    env_logger::init();
 }
