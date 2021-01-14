e4f45c3360a37d501e25ba60e6e432c27ead6592
YOSHIOKA Takuma <lo48576@hard-wi.red>
2021-01-15 06:56:25 +0900

    Bump `env_logger` to 0.8.2

--- src/bin/fbx-viewer/main.rs
+++ src/bin/fbx-viewer/main.rs
@@ -7,7 +7,7 @@ use structopt::StructOpt;
 pub mod vulkan;
 
 fn main() {
-    env_logger::from_env(env_logger::Env::default().default_filter_or("debug")).init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
     info!("version: {}", env!("CARGO_PKG_VERSION"));
 
     let opt = CliOpt::from_args();
